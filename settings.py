303c4fd664ac21b66a860da06527b04d9740771a
Ishafizan Ishak <ishafizan@gmail.com>
2025-02-12 13:26:34 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ishafizan'
+__date__: "12 Feb 2025"
+
+"""
+https://github.com/ishafizan/CRYPTO-BUY-SELL-HOLD-SIGNALS-AND-ALERTS/blob/main/README.md
+"""
+
+# ----------------------------------------------
+# Binance API Configuration (Public API)
+BINANCE_API_KEY = ""  # No need for a private key since we're only reading market data
+BINANCE_SECRET_KEY = ""
+# ----------------------------------------------
+# Telegram Configuration
+TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_CHAT_ID = ""
+# ----------------------------------------------
+# CRYPTOs to monitor
+SYMBOLS = ["BTCUSDT", "XRPUSDT", "WLDUSDT", "ETHUSDT", "SOLUSDT"]
