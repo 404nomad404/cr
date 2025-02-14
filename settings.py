46e1de5385bf8d1cef5754ea38378cb29e72cf4e
Ishafizan Ishak <ishafizan@gmail.com>
2025-02-14 15:10:33 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,6 @@
 __author__ = 'Ishafizan'
 __date__: "12 Feb 2025"
 
-"""
-https://github.com/ishafizan/CRYPTO-BUY-SELL-HOLD-SIGNALS-AND-ALERTS/blob/main/README.md
-"""
-
 # ----------------------------------------------
 # Binance API Configuration (Public API)
 BINANCE_API_KEY = ""  # No need for a private key since we're only reading market data
